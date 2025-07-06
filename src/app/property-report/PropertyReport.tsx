2aa93a6025998753877dfda603b6e9de0e2a7f4b
mosti <mosti4@hotmail.com>
2025-07-06 21:23:39 +0200

    Created a separat page-builder

---
+++ src/app/property-report/PropertyReport.tsx
@@ -0,0 +1,55 @@
+import {alpha, Box, CssBaseline, Stack} from "@mui/material";
+import AppTheme from "../shared-theme/AppTheme";
+import SideMenu from "@/page-builder/components/SideMenu";
+import {
+  chartsCustomizations,
+  dataGridCustomizations,
+  datePickersCustomizations,
+  treeViewCustomizations
+} from "@/app/dashboard/theme/customizations";
+import AppNavbar from "@/page-builder/components/AppNavbar";
+import Header from "@/page-builder/components/Header";
+import MainGrid from "@/page-builder/components/MainGrid";
+
+const xThemeComponents = {
+  ...chartsCustomizations,
+  ...dataGridCustomizations,
+  ...datePickersCustomizations,
+  ...treeViewCustomizations,
+};
+
+export default function propertyReport(props: { disableCustomTheme?: boolean }) {
+  return (
+      <AppTheme {...props} themeComponents={xThemeComponents}>
+        <CssBaseline enableColorScheme/>
+        <Box sx={{display: 'flex'}}>
+          <SideMenu/>
+          <AppNavbar/>
+          {/* Main content */}
+          <Box
+              component="main"
+              sx={(theme) => ({
+                flexGrow: 1,
+                backgroundColor: theme.vars
+                    ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+                    : alpha(theme.palette.background.default, 1),
+                overflow: 'auto',
+              })}
+          >
+            <Stack
+                spacing={2}
+                sx={{
+                  alignItems: 'center',
+                  mx: 3,
+                  pb: 5,
+                  mt: {xs: 8, md: 0},
+                }}
+            >
+              <Header/>
+              <MainGrid/>
+            </Stack>
+          </Box>
+        </Box>
+      </AppTheme>
+  );
+}
--- src/app/sales-report/SalesRapport.tsx
+++
@@ -1,9 +0,0 @@
-export default function SalesReportPage() {
-  return (
-    <main style={{ padding: 32 }}>
-      <h1>Sales Report</h1>
-      <p>This is the sales report page for BROKER (MEGLER).</p>
-    </main>
-  );
-}
-
